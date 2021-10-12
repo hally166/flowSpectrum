642c8b5c7bf09b8b1f541d73e54cde0afb340bcc
Christopher <christopher.hall@babraham.ac.uk>
2021-10-12 15:03:06 +0100

    added bins param

--- R/spectralplot.R
+++ R/spectralplot.R
@@ -9,6 +9,7 @@
 #' @param flowfile A flowSet or flowFrame
 #' @param theme Choose a theme: 'viridis' (default), 'bigfoot', or 'aurora'
 #' @param save FALSE: in console (default). TRUE : as png file in working directory
+#' @param bins Choose the granularity of the data.  Between 200 and 1000 works well for most data.
 #' @return Images of full spectrum
 #' @export
 spectralplot<-function(flowfile, theme='viridis', save=FALSE){
