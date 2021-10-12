2597532197810798c822e12474266f3c138d9560
Christopher <christopher.hall@babraham.ac.uk>
2021-10-12 14:49:54 +0100

    added fallback
    When an instrument is not recognised, the function will just take the area readings.   I think the only machine this will fail on is the cellstream.

--- R/spectralplottingtool.R
+++ R/spectralplottingtool.R
@@ -27,7 +27,10 @@ spectralplottingtool<-function(flowfile,theme='viridis', save=FALSE){
   } else if (flowfile@description$`$CYT`=="ID7000"){
     data2<-data[,-grep("SC", names(data))]
     data2<-data2[,grep("-A", names(data2))]
-  }
+  } else {
+    print("This FCS file is not from an Aurora, Bigfoot, or ID7000.  I will try and guess the relevant parameters...or I might just fail.")
+    data2<-data[,grep("-A", names(data))]
+  } 
   dat_long2 <- tidyr::pivot_longer(data2, cols =1:length(colnames(data2)))
   if (theme=='viridis'){
     p<-ggplot(dat_long2, aes(factor(name, level = as.list(unique(dat_long2['name']))$name), value) ) +
